fedd6474c4eafcdb213c1a8cd01b4f305b61464f
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2021-06-10 16:05:12 +0900

    test(sample_app): add an integration test for signup

---
+++ sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name: 'user',
+                                         email: 'user@valid',
+                                         password: 'foobar',
+                                         password_confirmation: 'foobar' } }
+    end
+  end
+
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+  end
+end
